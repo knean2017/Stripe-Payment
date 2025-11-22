5551f104cf5af7492d5ce584cfc970ba05ead7b1
knean2017 <mammadaliyevmammadali@gmail.com>
2025-11-23 01:21:48 +0400

    fix

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-2s(+ctluo+a(qhk3#v4_t
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
